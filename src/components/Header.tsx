82d6208239396899029f3501cf836d17c6e60416
Corning-AI <corning20@icloud.com>
2025-12-03 02:27:49 +0800

    Add CV download link to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -134,6 +134,22 @@ export default function Header() {
             </svg>
           </a>
         </li>
+        <li className="mr-5 shrink-0 text-xs">
+          <a
+            className="block text-slate-400 hover:text-teal-300"
+            href="/CV-Ning-Kang.pdf"
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="Download CV (opens in a new tab)"
+            title="CV / Resume"
+          >
+            <span className="sr-only">CV</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6" aria-hidden="true">
+              <path fillRule="evenodd" d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625ZM7.5 15a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 7.5 15Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H8.25Z" clipRule="evenodd" />
+              <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
+            </svg>
+          </a>
+        </li>
       </ul>
     </header>
   );
